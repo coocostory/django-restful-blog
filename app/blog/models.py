e258987b8d251ad90dcde22713583fbea0c05389
coocostory <583156031@qq.com>
2020-03-01 22:08:18 +0800

    comment

--- app/blog/models.py
+++ app/blog/models.py
@@ -8,6 +8,13 @@ class Blog(models.Model):
     title = models.CharField(max_length=100, default='default')
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         ordering = ['created']
+
+
+class Comment(models.Model):
+    comment_user = models.CharField(max_length=100, default='匿名')
+    comment = models.CharField(max_length=256, null=False)
+    likes = models.IntegerField(default=0)
